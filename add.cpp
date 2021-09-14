8ff412dbe709f8dc249705491209b438fcf31f12
7nidhi <52148582+7nidhi@users.noreply.github.com>
2021-09-14 17:13:09 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num_1,num_2,sum;
+	cout<<"enter the first num";
+	cin>>num_1;
+	cout<<"enter the second num";
+	cin>>num_2;
+	sum=num_1+num_2;
+	cout<<"sum of the number is :"<<sum; 
+	return 0;
+}
