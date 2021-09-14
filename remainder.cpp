65f567b7eb1db2b374c11272609b763fcd12ef67
7nidhi <52148582+7nidhi@users.noreply.github.com>
2021-09-14 17:14:09 +0530

    Find remainder

---
+++ remainder.cpp
@@ -0,0 +1,14 @@
+//find remainder and quationt
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,divider,remainder,quatient;
+cin>>num;
+cin>>divider;
+remainder=num/divider;
+quatient=num%divider;
+cout<<remainder<<endl;
+cout<<quatient;
+return 0;	
+}
