768c363425a34c85f2da226ac0122e929294d9c5
7nidhi <52148582+7nidhi@users.noreply.github.com>
2021-09-14 18:05:55 +0530

    Sum of natural numbers

---
+++ sum_naturalnum.cpp
@@ -0,0 +1,14 @@
+//sum of natural num
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,sum=0,i;
+	cout<<"enter the num";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+i;	
+	}
+	cout<<sum;
+}
