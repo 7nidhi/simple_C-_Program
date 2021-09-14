7ee685b6db24225aa2fc59ffdd0ad5d93e551890
7nidhi <52148582+7nidhi@users.noreply.github.com>
2021-09-14 17:15:41 +0530

    Find num is even or Odd

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+//check num is even or odd
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"num is even";
+	}
+	else
+	{
+		cout<<"num is odd";
+	}
+	return 0;
+}
