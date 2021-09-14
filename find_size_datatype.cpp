ab03839e7987a856ea761eb0e06d591c4d1da4c0
7nidhi <52148582+7nidhi@users.noreply.github.com>
2021-09-14 17:14:56 +0530

    Find the size of datatype

---
+++ find_size_datatype.cpp
@@ -0,0 +1,15 @@
+//find size of int float double char
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	cout<<"size of integer:"<<sizeof(int)<<endl;
+	
+	cout<<"size of float:"<<sizeof(char)<<endl;
+	
+	cout<<"size of double:"<<sizeof(double)<<endl;
+	
+	cout<<"size of char:"<<sizeof(float)<<endl;
+	return 0;
+}
