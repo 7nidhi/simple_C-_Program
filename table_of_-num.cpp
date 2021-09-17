8d60db49ed017344e74113d4e0cc6498ef797c11
7nidhi <52148582+7nidhi@users.noreply.github.com>
2021-09-17 16:21:19 +0530

    Multiplication table

---
+++ table_of_-num.cpp
@@ -0,0 +1,14 @@
+//multiplication table
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,i;
+	cout<<"enter  the num";
+	cin>>num;
+	for(i=1;i<=10;i++)
+	{
+		cout<<num*i<<endl;	
+	}
+	return 0;
+}
